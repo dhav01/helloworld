38337f1b16b74f7795e1a2a43cb80ef14024b520
dhaval <dhagrawal0@gmail.com>
2022-03-01 15:16:23 +0530

    updated settings for heroku

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const connectDB = require('./config/db')
 
 connectDB()
 
-app.listen(process.env.PORT, () => {
+const port = process.env.PORT || 3000
+app.listen(port, () => {
   console.log(`Server is running on port ${process.env.PORT}`)
 })
